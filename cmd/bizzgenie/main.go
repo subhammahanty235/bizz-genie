d16bcdf39dac5bcf01ed2af7fe14f8932a192f12
Subham <subhammahanty235@gmail.com>
2025-05-21 00:25:16 +0530

    basic setup of genie main

--- cmd/bizzgenie/main.go
+++ cmd/bizzgenie/main.go
@@ -1,1 +1,21 @@
-package bizzgenie
+package main
+
+import (
+	"fmt"
+
+	"github.com/subhammahanty235/bizzgenie/internal/redisclient"
+)
+
+type BizzGenie struct {
+	internalRedis *redisclient.NewInternalRedisClient
+	externalRedis *redisclient.NewExternalRedisClient
+}
+
+func main() {
+	fmt.Println("Bizz Genie is Running and Ready to process")
+	//handshake mechanism --> times based
+
+	//health checkup --> timer based on regular intervals
+
+	//redis instance health update --> while start
+}
