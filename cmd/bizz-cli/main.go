3c0d6a0151af83d80004bb325c3056c5717c8a19
Subham <subhammahanty235@gmail.com>
2025-05-21 00:10:55 +0530

    basic cli setup

--- cmd/bizz-cli/main.go
+++ cmd/bizz-cli/main.go
@@ -1,1 +1,45 @@
-package bizzcli
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	fmt.Println("BizzGenie CLI - Interactive Shell")
+	fmt.Println("Type 'help' for available commands or 'exit' to quit")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		args := strings.Fields(input)
+
+		if len(args) == 0 {
+			continue
+		}
+		switch strings.ToLower(args[0]) {
+		case "exit", "quit":
+			fmt.Println("GoodBye")
+			return
+		case "help":
+			fmt.Println("Helpeig")
+
+		default:
+			fmt.Println("Default is running")
+
+		}
+
+		printPrompt()
+
+	}
+
+}
+
+func printPrompt() {
+	color.New(color.FgHiCyan).Print("bizzcli> ")
+}
