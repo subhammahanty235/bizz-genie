4a5fe30547bc96a195d2dc0cb32ada71491d26e7
Subham <subhammahanty235@gmail.com>
2025-06-07 11:36:30 +0530

    basic grpc connection implmented

--- cmd/bizzgenie/main.go
+++ cmd/bizzgenie/main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 
-	"github.com/subhammahanty235/bizzgenie/internal/redisclient"
+	pb "github.com/subhammahanty235/bizzgenie/proto"
+	"google.golang.org/grpc"
 )
 
-type BizzGenie struct {
-	internalRedis *redisclient.NewInternalRedisClient
-	externalRedis *redisclient.NewExternalRedisClient
+type Server struct {
+	pb.UnimplementedHandShakeServer
+	pb.UnimplementedHeartBeatServer
 }
 
+// type BizzGenie struct {
+// 	// internalRedis *redisclient.NewInternalRedisClient()
+// 	externalRedis *redisclient.NewExternalRedisClient()
+// }
+
 func main() {
+
 	fmt.Println("Bizz Genie is Running and Ready to process")
+	//tcp connection
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen to tcp port: %v", err)
+	}
+
+	grpcserver := grpc.NewServer()
+	pb.RegisterHandShakeServer(grpcserver, &Server{})
+	pb.RegisterHeartBeatServer(grpcserver, &Server{})
+
 	//handshake mechanism --> times based
 
 	//health checkup --> timer based on regular intervals
 
 	//redis instance health update --> while start
+
+	//start the tcp connection
+	if err := grpcserver.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
